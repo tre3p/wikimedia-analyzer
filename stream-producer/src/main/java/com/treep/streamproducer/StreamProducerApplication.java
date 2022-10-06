aac17851f4baac8abdd38b7d451239c9152f5c44
tre3p <whereareyo@yahoo.com>
2022-10-06 22:06:05 +0300

    init producer

---
+++ stream-producer/src/main/java/com/treep/streamproducer/StreamProducerApplication.java
@@ -0,0 +1,13 @@
+package com.treep.streamproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamProducerApplication.class, args);
+    }
+
+}
---
+++ stream-producer/src/test/java/com/treep/streamproducer/StreamProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treep.streamproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
